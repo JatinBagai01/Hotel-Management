16df7f34446dc6c9dc72726e9d60414fe6e4e145
kanishka2411 <intern.kanishka@gigwalla.com>
2021-11-02 16:46:47 +0530

    Updated packages

--- header.php
+++ header.php
@@ -5,47 +5,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <title>Hotel Fantastic | Best Hotel in Rajshahi</title>
 
-<!-- Google fonts -->
-<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
-
-<!-- font awesome -->
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
 
 <!-- bootstrap -->
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-
-<!-- uniform -->
-<link type="text/css" rel="stylesheet" href="assets/uniform/css/uniform.default.min.css" />
-
-<!-- animate.css -->
-<link rel="stylesheet" href="assets/wow/animate.css" />
-
-
-<!-- gallery -->
-<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
-
-
-<!-- favicon -->
-<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-<link rel="icon" href="images/favicon.png" type="image/x-icon">
-
 <link rel="stylesheet" href="assets/style.css">
 
 </head>
 
 <body id="home">
-
-
-<!-- top 
-  <form class="navbar-form navbar-left newsletter" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Enter Your Email Id Here">
-        </div>
-        <button type="submit" class="btn btn-inverse">Subscribe</button>
-    </form>
- top -->
 
 <!-- header -->
 <nav class="navbar  navbar-inverse" role="navigation">
