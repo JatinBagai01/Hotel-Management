b19bc123b722123ac5b2f950f6e33912b0fc7d58
JatinBagai <jatin.bagai01@gmail.com>
2021-11-07 22:20:29 +0530

    First Draft

--- admin-login.php
+++ admin-login.php
@@ -9,6 +9,10 @@ include 'includes/titlebox.php';
 <?php
 // include connection
 include 'includes/connection.php';
+$fun_call = new Functions();
+$fetch_enquiry = $fun_call->array_make('enquiry');
+$fetch_reserve = $fun_call->array_make('reservation');
+
 
 // declare variables and intialize with empty values
 $loginIDErr = $passwordErr = "";
@@ -36,14 +40,14 @@ if (isset($_POST["submit"]) && !empty($_POST["submit"])) {
         // $myfilebin = fopen("bin.txt", "r") or die("Unable to open file!");
         // $txt;
         // if (fread($myfilebin, filesize("bin.txt")) == "0") {
-            $txt = "1";
+        $txt = "1";
         // }
         $myfile = fopen("bin.txt", "w") or die("Unable to open file!");
         fwrite($myfile, $txt);
         fclose($myfile);
         // fclose($myfilebin);
         // header('Location: index.php');
-        //                     exit;
+        // exit;
     }
 }
 
@@ -63,10 +67,10 @@ function test_input($data)
         <div class="row g-0">
             <div class="col-lg-8">
                 <div class="contact_form">
-                <?php
+                    <?php
                     $myfile = fopen("bin.txt", "r") or die("Unable to open file!");
                     if (fread($myfile, filesize("bin.txt")) == "0") {
-                        ?>
+                    ?>
                         <form method="POST" action="<?php echo htmlspecialchars(basename($_SERVER["PHP_SELF"])); ?>#" id="form">
                             <div class="row">
                                 <div class="col-lg-8">
@@ -92,58 +96,70 @@ function test_input($data)
                         <table class="table">
                             <thead class="thead-dark">
                                 <tr>
-                                <th scope="col">Firstname</th>
-                                <th scope="col">Lastname</th>
-                                <th scope="col">Email ID</th>
-                                <th scope="col">Contact Number</th>
-                                <th scope="col">Messages</th>
+                                    <th scope="col">Firstname</th>
+                                    <th scope="col">Lastname</th>
+                                    <th scope="col">Email ID</th>
+                                    <th scope="col">Contact Number</th>
+                                    <th scope="col">Messages</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>fname from db</td>
-                                    <td>lname from db</td>
-                                    <td>email from db</td>
-                                    <td>contact from db</td>
-                                    <td>message from db</td>
-                                </tr>
+                                <?php
+                                if ($fetch_enquiry) {
+                                    foreach ($fetch_enquiry as $adata) {
+                                        echo "<tr>";
+                                        echo "<td>" . $adata['firstname'] . "</td>";
+                                        echo "<td>" . $adata['lastname'] . "</td>";
+                                        echo "<td>" . $adata['email'] . "</td>";
+                                        echo "<td>" . $adata['mobile'] . "</td>";
+                                        echo "<td>" . $adata['message'] . "</td>";
+                                        echo "</tr>";
+                                    }
+                                }
+                                ?>
                             </tbody>
                         </table>
                         <input type="submit" name="submit" value="Reservations" class="btn btn_theme shadow-none" style="margin-top:30px;" onclick="on()">
                         <table class="table">
                             <thead class="thead-dark">
                                 <tr>
-                                <th scope="col">Firstname</th>
-                                <th scope="col">Lastname</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Contact</th>
-                                <th scope="col">Age</th>
-                                <th scope="col">Alternate Number</th>
-                                <th scope="col">No. of Adults</th>
-                                <th scope="col">No. of Children</th>
-                                <th scope="col">Check In Date</th>
-                                <th scope="col">Check Out Date</th>
-                                <th scope="col">Type of Room</th>
-                                <th scope="col">No. of Rooms</th>
-                                <th scope="col">Special Request</th>
+                                    <th scope="col">Firstname</th>
+                                    <th scope="col">Lastname</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Contact</th>
+                                    <th scope="col">Age</th>
+                                    <th scope="col">Alternate Number</th>
+                                    <th scope="col">No. of Adults</th>
+                                    <th scope="col">No. of Children</th>
+                                    <th scope="col">Check In Date</th>
+                                    <th scope="col">Check Out Date</th>
+                                    <th scope="col">Type of Room</th>
+                                    <th scope="col">No. of Rooms</th>
+                                    <th scope="col">Special Request</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>fname from db</td>
-                                    <td>lname from db</td>
-                                    <td>email from db</td>
-                                    <td>contact from db</td>
-                                    <td>age from db</td>
-                                    <td>alternate from db</td>
-                                    <td>no of adults from db</tb>
-                                    <td>no of children from db</tb>
-                                    <td>checkin date from db</td>
-                                    <td>checkout date from db</td>
-                                    <td>typeofroom from db</tb>
-                                    <td>no of room from db</tb>
-                                    <td>request from db</td>
-                                </tr>
+                                <?php
+                                if ($fetch_reserve) {
+                                    foreach ($fetch_reserve as $adata) {
+                                        echo "<tr>";
+                                        echo "<td>" . $adata['firstname'] . "</td>";
+                                        echo "<td>" . $adata['lastname'] . "</td>";
+                                        echo "<td>" . $adata['email'] . "</td>";
+                                        echo "<td>" . $adata['mobile'] . "</td>";
+                                        echo "<td>" . $adata['age'] . "</td>";
+                                        echo "<td>" . $adata['alternate'] . "</td>";
+                                        echo "<td>" . $adata['adults'] . "</td>";
+                                        echo "<td>" . $adata['children'] . "</td>";
+                                        echo "<td>" . $adata['checkin'] . "</td>";
+                                        echo "<td>" . $adata['checkout'] . "</td>";
+                                        echo "<td>" . $adata['roomtype'] . "</td>";
+                                        echo "<td>" . $adata['room'] . "</td>";
+                                        echo "<td>" . $adata['message'] . "</td>";
+                                        echo "</tr>";
+                                    }
+                                }
+                                ?>
                             </tbody>
                         </table>
                         <a href="<?php echo BASE_URL; ?>logout.php"><input type="submit" name="submit" value="Logout" class="btn btn_theme shadow-none"></a>
--- includes/connection.php
+++ includes/connection.php
@@ -11,3 +11,30 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+
+class Functions{
+
+public function array_make($tbl_name)
+{
+  $hostname = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "hotel";
+  $conn = mysqli_connect($hostname, $username, $password, $database);
+  $select = "SELECT * FROM $tbl_name";
+  $query = mysqli_query($conn, $select);
+  if (mysqli_num_rows($query) > 0) {
+    $select_fetch = mysqli_fetch_all($query, MYSQLI_ASSOC);
+    if ($select_fetch) {
+      return $select_fetch;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+}
+
+?>
